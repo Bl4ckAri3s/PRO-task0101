b8db9ca2702847a28c4f71780ce9cbee83562577
Alex <ariesb6343@gmail.com>
2020-08-05 19:28:27 +0200

    Finished

--- main.cpp
+++ main.cpp
@@ -18,8 +18,16 @@ int main() {
     cin >> input;
 
     // Output all prime numbers smaller or equal to input number
+    cout << "Prastevila, ki so manjsa ali enaka stevilu " << input << ": ";
     for (int i = 2; i <= input; i++) {
         if (is_prime(i))
+            cout << i << ", ";
+    }
+
+    // Output multiples of 3 and 5, smaller than input number
+    cout << "\nStevila, ki so veckratniki stevil 3 in 5 ter so manjsa od stevila " << input << ": ";
+    for (int i = 1; i < input; i++) {
+        if (i % 3 == 0 && i % 5 == 0)
             cout << i << ", ";
     }
 
