654fdc5f63a0aea4f8cb01fbb5e89509d89e6d30
Alex <ariesb6343@gmail.com>
2020-08-05 17:03:51 +0200

    starting new project

---
+++ cmake-build-debug/CMakeFiles/task0101.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task0101.dir/main.cpp.o"
+  "task0101"
+  "task0101.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task0101.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/task0101.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for task0101.
+# This may be replaced when dependencies are built.
