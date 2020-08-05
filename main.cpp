800924a072da290ab8d9c5eadffbe60927769170
Alex <ariesb6343@gmail.com>
2020-08-05 17:21:59 +0200

    First part of the task done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+using namespace std;
+
+
+bool is_prime(const int& number) {
+    for (int i = 2; i < number; i++) {
+        if (number % i == 0) return false;
+    }
+    return true;
+}
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int input;
+
+    cout << "Hello! Enter a positive number: ";
+    cin >> input;
+
+    // Output all prime numbers smaller or equal to input number
+    for (int i = 2; i <= input; i++) {
+        if (is_prime(i))
+            cout << i << ", ";
+    }
+
+
+
     return 0;
 }
